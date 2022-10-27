362142e4252498049cc7171864d2bd0b5dc4cd24
Антон <@DESKTOP-HRMH2DT>
2022-10-27 22:39:29 +0300

    Добавьте файлы проекта.

---
+++ config.py
@@ -0,0 +1,10 @@
+#Токен вашего бота
+bot_token = '5723187834:AAGZrk_AY12f65lH7SkkuM83oexZRizsyGs'
+#ID админа
+admin = 5799344728
+#Юзер саппорта
+support = '@Username'
+#Канал с отзывами
+#feedback = 'https://t.me/username'
+#Название магазина
+shop_name = 'Shop'
